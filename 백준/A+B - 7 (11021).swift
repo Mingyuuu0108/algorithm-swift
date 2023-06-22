1d96356021b5c640cbd3ee59c89c2a193c54bc50
Mingyuuu0108 <dlalsrb@dgsw.hs.kr>
2023-06-22 13:29:29 +0900

    UPDATE

--- 백준/A+B - 7 (11021).swift
+++ 백준/A+B - 7 (11021).swift
@@ -1,6 +1,6 @@
 let n = Int(readLine()!)!
 
-for i in 1...n+1 {
+for i in 1...n {
     let numbers = readLine()!.split(separator: " ").compactMap { Int($0) }
     print("Case #\(i): \(numbers.reduce(0, +))")
 }
