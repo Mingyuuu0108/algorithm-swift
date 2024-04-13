8477d7f0fdd658c5c4617ad73f27795224dfbe17
이민규 <104059751+Mingyuuu0108@users.noreply.github.com>
2024-04-13 21:44:50 +0900

    Update 옹알이 (1).swift

--- 프로그래머스/옹알이 (1).swift
+++ 프로그래머스/옹알이 (1).swift
@@ -1,6 +1,18 @@
 import Foundation
 
+/// 새로운 풀이
 func solution(_ babbling:[String]) -> Int {
+    return babbling.filter { babble in
+        let replace = babble.replacingOccurrences(of: "aya", with: " ")
+                            .replacingOccurrences(of: "ye", with: " ")
+                            .replacingOccurrences(of: "woo", with: " ")
+                            .replacingOccurrences(of: "ma", with: " ")
+        return replace.trimmingCharacters(in: .whitespaces).isEmpty
+    }.count
+}
+
+/// 예전 풀이
+func oldSolution(_ babbling:[String]) -> Int {
     
     var replaceArray: [String] = []
     var (index, result) = (0, 0)
